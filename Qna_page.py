6623a6e2312df1258525e00d1012ab215d87df65
virag jain <viragjain2345.gmil.com>
2019-07-07 16:17:56 +0530

    added files

---
+++ Qna_page.py
@@ -0,0 +1,11 @@
+#needs to be edited
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to our forum!'
+`	
---
+++ run_app.py
@@ -0,0 +1,10 @@
+#Needs to be edited..
+import os
+
+from bla_bla import bla_bla
+
+if __name__ == '__main__':
+    app.debug = True
+    host = os.environ.get('IP', '0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)
