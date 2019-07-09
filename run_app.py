67de8ec6e4be1689eabf0dbf8242695efa60a08c
virag jain <viragjain2345.gmil.com>
2019-07-09 15:15:56 +0530

    Okay connected login form

--- run_app.py
+++ run_app.py
@@ -1,20 +1,41 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField , PasswordField	 , BooleanField
+from wtforms.validators import InputRequired , Email, Length
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'virag'
+
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	remember = BooleanField('remember me')
+
+class SignUpForm(FlaskForm):
+	email    = StringField('email' , validators=[InputRequired() , Length(max=50) , Email(message='Invalid Email') ])
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/Home')
 def Home():
 	return render_template('Home.html')
 
-@app.route('/Login')
-def Loginx():
-	return render_template('Login.html')
+@app.route('/')
+def Login():
+	form = LoginForm() 
+
+	return render_template('Login.html' , form=form)
 
 @app.route('/Signup')
 def Signup():
-	return render_template('Signup.html')
+	form = SignupForm() 
+
+
+	return render_template('Signup.html' , form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
