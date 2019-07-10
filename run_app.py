aff2f0fb34bda345ab842446bcedc9b2a28f473a
virag jain <viragjain2345.gmil.com>
2019-07-10 19:00:45 +0530

    Connected database to feed page

--- run_app.py
+++ run_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField , PasswordField	 , BooleanField
+from wtforms.widgets import TextArea
 from wtforms.validators import InputRequired , Email, Length
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,15 +10,33 @@ app = Flask(__name__)
 Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'virag'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/DELL/Desktop/Flask_forum/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_BINDS'] = {'sawaal' : 'sqlite:///sawaal.db' , 
+									'jawaab' : 'sqlite:///jawaab.db' }
+
 
 db = SQLAlchemy(app)
+
+class PostForm(FlaskForm):
+    question = StringField('question', validators=[InputRequired(), Length(min=1, max=200)])
+    answer   = StringField('answer', widget=TextArea())
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(15), unique=True)
 	email = db.Column(db.String(50), unique=True)
 	password = db.Column(db.String(80))
+
+class sawaal(db.Model):
+	__bind_key__ = 'sawaal'
+	id = db.Column(db.Integer, primary_key=True)		
+	question = db.Column(db.String(200))
+
+class jawaab(db.Model):
+	__bind_key__ = 'jawaab'
+	id = db.Column(db.Integer, primary_key=True)		
+	answer = db.Column(db.String(200))
+
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -32,8 +51,8 @@ class SignUpForm(FlaskForm):
 
 @app.route('/')
 def Home():
-	form = SignUpForm()
-	return render_template('Signup.html' , form=form)
+	form = PostForm()
+	return render_template('Feed.html' , form=form)
 
 
 @app.route('/Login'  , methods=['GET' , 'POST'])
@@ -45,8 +64,6 @@ def Login():
 			if user.password == form.password.data:
 			  	return redirect(url_for('Home'))
 		return '<h1>Invalid username or password</h1>'
-        
-		#return '<h1>' + "USERNAME" + form.username.data + ' ' + " PASSWORD "+form.password.data + '</h1>'
 
 	return render_template('Login.html' , form=form)
 
@@ -55,12 +72,28 @@ def Signup():
 	form = SignUpForm() 
 	if form.validate_on_submit():
 		new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-		db.session.add(new_user)
+		db.session.add(new_user)	
 		db.session.commit()
 		return '<h1> New user is created</h1>'
 		#return '<h1>' + "USERNAME" + form.username.data + ' ' + " PASSWORD "+form.password.data + " Email "+form.email.data + '</h1>'
 
 	return render_template('Signup.html' , form=form)
+
+@app.route('/heyhey', methods=['GET' , 'POST'])
+def Feed():
+	form = PostForm()
+	if form.validate_on_submit():
+		new_question = sawaal(question = form.question.data)
+		db.session.add(new_question)	
+		db.session.commit()
+
+		new_answer = jawaab(answer = form.answer.data)
+		db.session.add(new_answer)	
+		db.session.commit()
+
+	return '<h1> New question is created</h1>'
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
